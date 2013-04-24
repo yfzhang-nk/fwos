09c2da041ebb9ea4533f44c3271276b2697d1d11
Yifei Zhang <yfzhang@yfzhang-ThinkPad-T61.(none)>
2013-04-25 00:25:12 +0800

    increase resolution to 800*600*8 bit color

--- asmhead.asm
+++ asmhead.asm
@@ -1,6 +1,7 @@
 ; haribote-os boot asm
 ; TAB=4
 [SECTION .data]
+VBEMODE EQU 0x103
 BOTPAK	EQU 0x00280000
 DSKCAC	EQU 0x00100000		
 DSKCAC0	EQU 0x00008000		
@@ -21,14 +22,68 @@ VRAM	EQU 0x0ff8 ;图像缓冲区的开始地址
 [SECTION .head]
 start:
 	ORG 0c200h
-	MOV AL, 0x13 ; VGA显卡
-	MOV AH, 0x00
+	; 确认VBE是否存在
+	MOV AX, 0x9000
+	MOV ES, AX
+	MOV DI, 0
+	MOV AX, 0x4f00
+	INT 0x10
+	CMP AX, 0x004f
+	JNE scrn320
+
+	;检查VBE版本
+	MOV AX, [ES:DI+4]
+	CMP AX, 0x0200
+	JB scrn320
+	
+	;取得画面模式信息
+	MOV CX, VBEMODE
+	MOV AX, 0x4f01
+	INT 0x10
+	CMP AX, 0x004f
+	JNE scrn320
+
+; WORD [ES : DI+0x00] : 模式属性 
+; WORD [ES : DI+0x12] : X的分辨率
+; WORD [ES : DI+0x14] : Y的分辨率
+; WORD [ES : DI+0x19] : 颜色数目
+; WORD [ES : DI+0x1b] : 颜色的指定方法
+; WORD [ES : DI+0x28] : VRAM的地址
+	; 画面模式信息的确认
+	CMP BYTE [ES:DI+0x19], 8
+	JNE scrn320
+	CMP BYTE [ES:DI+0x1b], 4
+	JNE scrn320
+	MOV AX, [ES:DI+0x00]
+	AND AX, 0x0080
+	JZ scrn320
+
+; 0x101 ------ 640 * 480 * 8bit color
+; 0x103 ------ 800 * 600 * 8bit color
+; 0x105 ------ 1024 * 768 * 8bit color
+; 0x107 ------ 1280 * 1024 * 8bit color
+	;画面模式切换
+	MOV BX, VBEMODE+0x4000
+	MOV AX, 0x4f02
+	INT 0x10
+	MOV BYTE [VMODE], 8
+	MOV AX, [ES:DI+0x12]
+	MOV [SCRNX], AX
+	MOV AX, [ES:DI+0x14]
+	MOV [SCRNY], AX
+	MOV EAX, [ES:DI+0x28]
+	MOV [VRAM], EAX
+	JMP keystatus
+
+scrn320
+	MOV AX, 0x0013
 	INT 0x10
 	MOV	BYTE [VMODE], 8 ;记录画面模式
-	MOV WORD [SCRNX], 320
-	MOV WORD [SCRNY], 200
+	MOV WORD [SCRNX], 320 
+	MOV WORD [SCRNY], 200 
 	MOV DWORD [VRAM], 0x000a0000
 
+keystatus:
 ; 用BIOS取得键盘上各种LED指示灯的状态
 	MOV AH, 0x02
 	INT 0x16 ; keyboard BIOS
